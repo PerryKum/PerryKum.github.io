ee9a85c31362758228718a0c8d2d9eef088b7141
PerryKum <2273157479@qq.com>
2023-07-21 23:02:30 +0800

    修改ul自适应

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const setNewsHeight = () => {
+    document.getElementById('news').style.height = 0.5*document.getElementById('news').offsetWidth + 'px';
+}
+
+setNewsHeight();
+addEventListener('resize',setNewsHeight);
+
 // Swipers
 var swiper1 = new Swiper('#swiper1', {
     autoplay: true,
